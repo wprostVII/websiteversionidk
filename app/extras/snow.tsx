536c232fc7cd6b6c16e099f8be99f060ae3d7a8b
wprostVII <192258874+wprostVII@users.noreply.github.com>
2025-10-23 03:05:47 +0200

    Fixed the snow positioning and timing.

--- app/extras/snow.tsx
+++ app/extras/snow.tsx
@@ -2,7 +2,7 @@
 
 export default function SnowFalling() {
   return (
-      <div className="w-screen h-screen text-6xl text-white">
+      <div className="fixed -top-30 w-screen h-screen text-6xl text-white">
         <div className="marqueeDown float-left p-1" style={{animationDelay: '0,5s', animationDuration: '8s'}}>.</div>
         <div className="marqueeDown float-left p-1" style={{animationDelay: '1s', animationDuration: '5s'}}>.</div>
         <div className="marqueeDown float-left p-1" style={{animationDelay: '4.5s', animationDuration: '7s'}}>.</div>
@@ -31,24 +31,24 @@ export default function SnowFalling() {
         <div className="marqueeDown float-left p-1" style={{animationDelay: '3.5s', animationDuration: '9s'}}>.</div>
 
 
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '0,5s', animationDuration: '8s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '1s', animationDuration: '5s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '4.5s', animationDuration: '7s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '2s', animationDuration: '8s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '7.5s', animationDuration: '10s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '3s', animationDuration: '7s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '3.5s', animationDuration: '9s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '4s', animationDuration: '6s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '2,5s', animationDuration: '7s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '0s', animationDuration: '9s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '6.5s', animationDuration: '5s'}}>.</div>
         <div className="marqueeDown float-right p-1" style={{animationDelay: '1.5s', animationDuration: '11s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '9s', animationDuration: '5s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '5.5s', animationDuration: '12s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '6s', animationDuration: '7s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '0.5s', animationDuration: '6s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '7s', animationDuration: '10s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '2.5s', animationDuration: '8s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '8s', animationDuration: '12s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '8.5s', animationDuration: '11s'}}>.</div>
-        <div className="marqueeDown float-right p-1" style={{animationDelay: '5s', animationDuration: '5s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '3.5s', animationDuration: '6s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '6s', animationDuration: '12s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '8.5s', animationDuration: '5s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '3s', animationDuration: '7s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '7.5s', animationDuration: '9s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '2s', animationDuration: '10s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '1.5s', animationDuration: '5s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '4s', animationDuration: '12s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '0.5s', animationDuration: '11s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '6s', animationDuration: '6s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '2.5s', animationDuration: '7s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '4s', animationDuration: '6s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '7.5s', animationDuration: '9s'}}>.</div>
+        <div className="marqueeDown float-right p-1" style={{animationDelay: '9s', animationDuration: '10s'}}>.</div>
       </div>
   );
 }
