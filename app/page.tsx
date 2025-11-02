675ce8677a4dc44589a72fb268d377c951c89410
wprostVII <192258874+wprostVII@users.noreply.github.com>
2025-11-02 19:40:28 +0100

    Removed the text shadow when the screen changes to desktop.

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Enter() {
           
           <div className="absolute bottom-20 right-5 md:right-10 text-right text-custom-black z-40">
 
-            <p className="font-['angelWish'] text-shadow-(--custom-shadow) text-[6rem] md:text-[8rem]">wprostVII</p>
-            <p className="font-['newScience'] text-shadow-(--custom-shadow) text-lg md:text-xl">also known as "Cement Mix"</p>
+            <p className="font-['angelWish'] text-shadow-(--custom-shadow) sm:text-shadow-none text-[6rem] md:text-[8rem]">wprostVII</p>
+            <p className="font-['newScience'] text-shadow-(--custom-shadow) sm:text-shadow-none text-lg md:text-xl">also known as "Cement Mix"</p>
             
-            <Link href="/home" className="mr-5 font-['loosBlack'] text-shadow-(--custom-shadow) hover:text-crimson-maybe">Enter</Link>
+            <Link href="/home" className="mr-5 font-['loosBlack'] text-shadow-(--custom-shadow) sm:text-shadow-none  hover:text-crimson-maybe">Enter</Link>
       
           </div>
 
-          <img src="/wiggle.webp" alt="knight standing" className="overflow-x-hidden fixed min-h-[50dvh] max-h-screen min-w-[50dvh] max-w-fit top-0 left-[-20vw] md:inset-0"/>
+          <img src="/wiggle.webp" alt="knight standing" className="overflow-x-hidden fixed min-h-[50dvh] max-h-screen min-w-[50dvh] max-w-fit top-0 left-[-50vw] sm:left-[-20vw] md:inset-0"/>
           
         </div>
 
