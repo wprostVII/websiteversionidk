6152bb688250d9d40effde44e71d59fd35a44a0f
wprostVII <192258874+wprostVII@users.noreply.github.com>
2025-12-26 22:53:57 +0100

    Added more snowflakes.

--- app/extras/snow.tsx
+++ app/extras/snow.tsx
@@ -17,21 +17,28 @@ export default function SnowFalling() {
         <div className="fixed left-50 marqueeDown float-left p-1" style={{animationDelay: '9s', animationDuration: '5s'}}><SnowSymbols/></div>
         <div className="fixed left-55 marqueeDown float-left p-1" style={{animationDelay: '5.5s', animationDuration: '12s'}}><SnowSymbols/></div>
         <div className="fixed left-60 marqueeDown float-left p-1" style={{animationDelay: '6s', animationDuration: '7s'}}><SnowSymbols/></div>   
-
         <div className="fixed left-65 marqueeDown float-left p-1" style={{animationDelay: '0.5s', animationDuration: '6s'}}><SnowSymbols/></div>
         <div className="fixed left-70 marqueeDown float-left p-1" style={{animationDelay: '7s', animationDuration: '10s'}}><SnowSymbols/></div>
         <div className="fixed left-75 marqueeDown float-left p-1" style={{animationDelay: '2.5s', animationDuration: '8s'}}><SnowSymbols/></div>
         <div className="fixed left-80 marqueeDown float-left p-1" style={{animationDelay: '8s', animationDuration: '12s'}}><SnowSymbols/></div>
         <div className="fixed left-85 marqueeDown float-left p-1" style={{animationDelay: '8.5s', animationDuration: '11s'}}><SnowSymbols/></div>
         
+        <div className="fixed left-90 marqueeDown float-left p-1" style={{animationDelay: '2s', animationDuration: '7s'}}><SnowSymbols/></div>
+        <div className="fixed left-95 marqueeDown float-left p-1" style={{animationDelay: '5.5s', animationDuration: '8s'}}><SnowSymbols/></div>
+        <div className="fixed left-100 marqueeDown float-left p-1" style={{animationDelay: '3s', animationDuration: '10s'}}><SnowSymbols/></div>
+        <div className="fixed left-105 marqueeDown float-left p-1" style={{animationDelay: '4.5s', animationDuration: '6s'}}><SnowSymbols/></div>
+        
+        <div className="fixed right-90 marqueeDown float-left p-1" style={{animationDelay: '2s', animationDuration: '7s'}}><SnowSymbols/></div>
+        <div className="fixed right-95 marqueeDown float-left p-1" style={{animationDelay: '5.5s', animationDuration: '8s'}}><SnowSymbols/></div>
+        <div className="fixed right-100 marqueeDown float-left p-1" style={{animationDelay: '3s', animationDuration: '10s'}}><SnowSymbols/></div>
+        <div className="fixed right-105 marqueeDown float-left p-1" style={{animationDelay: '4.5s', animationDuration: '6s'}}><SnowSymbols/></div>
+        
+        
         <div className="fixed right-85 marqueeDown float-right p-1" style={{animationDelay: '1s', animationDuration: '5s'}}><SnowSymbols/></div>
         <div className="fixed right-80 marqueeDown float-right p-1" style={{animationDelay: '4.5s', animationDuration: '7s'}}><SnowSymbols/></div>
         <div className="fixed right-75 marqueeDown float-right p-1" style={{animationDelay: '2s', animationDuration: '8s'}}><SnowSymbols/></div>
         <div className="fixed right-70 marqueeDown float-right p-1" style={{animationDelay: '7.5s', animationDuration: '10s'}}><SnowSymbols/></div>
         <div className="fixed right-65 marqueeDown float-right p-1" style={{animationDelay: '3s', animationDuration: '7s'}}><SnowSymbols/></div>
-        
-
-
         <div className="fixed right-60 marqueeDown float-right p-1" style={{animationDelay: '2,5s', animationDuration: '7s'}}><SnowSymbols/></div>
         <div className="fixed right-55 marqueeDown float-right p-1" style={{animationDelay: '0s', animationDuration: '9s'}}><SnowSymbols/></div>
         <div className="fixed right-50 marqueeDown float-right p-1" style={{animationDelay: '6.5s', animationDuration: '5s'}}><SnowSymbols/></div>
